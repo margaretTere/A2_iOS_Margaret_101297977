982523b31dbd1d1e249e91266243740da59c7dd1
margaretTere <may.terechenko@gmail.com>
2025-03-21 11:18:16 -0400

    Started new project

---
+++ A2_iOS_Margaret_101297977/A2_iOS_Margaret_101297977/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_iOS_Margaret_101297977
+//
+//  Created by Rita T on 2025-03-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ A2_iOS_Margaret_101297977/A2_iOS_Margaret_101297977Tests/A2_iOS_Margaret_101297977Tests.swift
@@ -0,0 +1,17 @@
+//
+//  A2_iOS_Margaret_101297977Tests.swift
+//  A2_iOS_Margaret_101297977Tests
+//
+//  Created by Rita T on 2025-03-21.
+//
+
+import Testing
+@testable import A2_iOS_Margaret_101297977
+
+struct A2_iOS_Margaret_101297977Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
