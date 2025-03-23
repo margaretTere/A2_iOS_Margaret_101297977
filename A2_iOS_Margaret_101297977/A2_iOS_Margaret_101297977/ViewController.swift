413a2c379226758bace1dd866a2f1af6589be5ec
margaretTere <may.terechenko@gmail.com>
2025-03-23 17:30:05 -0400

    Generated initial data

--- A2_iOS_Margaret_101297977/A2_iOS_Margaret_101297977/ViewController.swift
+++ A2_iOS_Margaret_101297977/A2_iOS_Margaret_101297977/ViewController.swift
@@ -6,12 +6,36 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+       
+      for i in 1...10 {
+            initalDataLoad(i, context: context)
+        }
+   
+    }
+    
+    func initalDataLoad(_ id: Int, context: NSManagedObjectContext) {
+        
+        let newProduct = NSEntityDescription.insertNewObject(forEntityName: "Product", into: context)
+        
+        newProduct.setValue(id, forKey: "productId")
+        newProduct.setValue("Product #\(id)", forKey: "productName")
+        newProduct.setValue("Added product #\(id)", forKey: "productDescription")
+        newProduct.setValue(7.95+Double(id), forKey: "productPrice")
+        newProduct.setValue("Supplier #\(id)", forKey: "productProvider")
+        
+        appDelegate.saveContext()
     }
 
 
