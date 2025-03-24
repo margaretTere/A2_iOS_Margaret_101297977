46eafd3f071d36b1998878889d2f0737a4377eeb
margaretTere <may.terechenko@gmail.com>
2025-03-24 14:10:43 -0400

    Got search working

--- A2_iOS_Margaret_101297977/A2_iOS_Margaret_101297977/SearchProductViewController.swift
+++ A2_iOS_Margaret_101297977/A2_iOS_Margaret_101297977/SearchProductViewController.swift
@@ -7,23 +7,55 @@
 
 import UIKit
 
-class SearchProductViewController: UIViewController {
+class SearchProductViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     var products: [Product]?
+    var filteredProducts: [Product]?
     
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchCriteria: UITextField!
-    
     @IBOutlet weak var btnSearch: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     
 
     @IBAction func searchProducts(_ sender: UIButton) {
-        print("\(products![0].productDescription ?? "No products")")
+        guard let searchText = searchCriteria.text?.lowercased(), !searchText.isEmpty
+        else {
+            filteredProducts = products ?? []
+            tableView.reloadData()
+            return
+        }
+        
+        filteredProducts = products?.filter { product in
+            let matchesName = product.productName?.lowercased().contains(searchText) ?? false
+            let matchesDescription = product.productDescription?.lowercased().contains(searchText) ?? false
+            return matchesName || matchesDescription
+            
+        }
+        
+        tableView.reloadData()
+        
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredProducts?.count ?? 0
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchCell", for: indexPath)
+        let product = filteredProducts?[indexPath.row]
+        cell.textLabel?.text = product?.productName ?? "No name"
+        cell.detailTextLabel?.text = product?.productDescription ?? "No description"
+            return cell
+        }
+    
     /*
     // MARK: - Navigation
 
